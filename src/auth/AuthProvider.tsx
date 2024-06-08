65321e1ce67d9f70ecacbc06d5dffc8c76da507c
Amirthanathan R <amirth300324@gmail.com>
2024-06-08 17:46:23 +0530

    Added auth provider

---
+++ src/auth/AuthProvider.tsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import {
+    createContext,
+    useEffect,
+    useMemo,
+    useState,
+
+    ReactNode,
+    useContext
+} from "react";
+
+
+const AuthContext = createContext();
+
+const TOKENKEY = 'token';
+
+type Prop = {children: ReactNode};
+const AuthProvider = ({children}: Prop) => {
+
+    const [token, setTokenInner] = useState(localStorage.getItem(TOKENKEY));
+
+    useEffect(() => {
+        if (token) {
+            axios.defaults.headers.common.Authorization = token;
+            localStorage.setItem('token', token);
+        } else {
+            delete axios.defaults.headers.common.Authorization;
+            localStorage.removeItem('token');
+        }
+    }, [token]);
+
+    const setToken = (newToken: string) => {
+        setTokenInner(newToken);
+    };
+    const contextValue = useMemo(() => ({
+        token,
+        setToken
+    }), [token]);
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+export default AuthProvider;
