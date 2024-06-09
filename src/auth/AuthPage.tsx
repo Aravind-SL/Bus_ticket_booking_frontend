03b4290d9d18b89e2220f8127664232917112ead
Amirthanathan R <amirth300324@gmail.com>
2024-06-09 09:55:30 +0530

    setup the layout for background image

--- src/auth/AuthPage.tsx
+++ src/auth/AuthPage.tsx
@@ -7,19 +7,21 @@ import SignUpForm from  './SignUpForm';
 */
 export default function Auth() {
     return (
-        <div className="w-full h-screen container flex items-center justify-center lg:justify-start">
-            <Tabs defaultValue="login" className="w-[400px]">
-                <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="login">Log In</TabsTrigger>
-                    <TabsTrigger value="signup">Sign Up</TabsTrigger>
-                </TabsList>
-                <TabsContent value="login">
-                    <LoginForm />
-                </TabsContent>
-                <TabsContent value="signup">
-                    <SignUpForm />
-                </TabsContent>
-            </Tabs>
+        <div className="w-full h-screen flex md:px-32 items-center justify-center md:justify-start bg-[url('https://via.placeholder.com/1920x1080')] bg-top-right bg-cover">
+            <div className="h-full md:px-6  lg:px-12 flex items-center bg-background/50 backdrop-blur w-full md:w-auto lg:border-x-2 border-secondary">
+                <Tabs defaultValue="login" className="w-[400px] mx-auto">
+                    <TabsList className="grid w-full grid-cols-2">
+                        <TabsTrigger value="login">Log In</TabsTrigger>
+                        <TabsTrigger value="signup">Sign Up</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="login">
+                        <LoginForm />
+                    </TabsContent>
+                    <TabsContent value="signup">
+                        <SignUpForm />
+                    </TabsContent>
+                </Tabs>
+            </div>
         </div>
     )
 }
