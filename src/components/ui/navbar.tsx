87bdd7d21fb5317c474448e7405c09c8cb173228
Sanjaycsk18 <sanjaycsk2004@gmail.com>
2024-06-19 23:00:33 +0530

    added responsiveness

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,17 +4,26 @@ import {
     MenubarMenu,
   } from '@/components/ui/menubar';
 import { Button } from '@/components/ui/button';
+import { useState } from 'react'; 
+import { redirect } from 'react-router-dom';
+
   
 export default function Navbar() {
+  const [Logged] = useState(false);
     return (
-      <Menubar className='h-250'>
+      <Menubar className='w-full h-250 flex items-center py-2 '>
         <MenubarMenu>
-            <MenubarTrigger>logo</MenubarTrigger>
+            <MenubarTrigger className='flex-auto md:flex-auto' >logo</MenubarTrigger>
             <MenubarTrigger>Home</MenubarTrigger>
             <MenubarTrigger>Browse</MenubarTrigger>
             <MenubarTrigger>Contact</MenubarTrigger>
-            <MenubarTrigger><Button >Log In / Sign Up</Button></MenubarTrigger>
+            {
+              Logged && <MenubarTrigger>Profile</MenubarTrigger>
+            
+            }
+            <MenubarTrigger><Button>Log In / Sign Up</Button></MenubarTrigger>
         </MenubarMenu>
       </Menubar>
     );
 }
+
