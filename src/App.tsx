2c0230d5b74d22d57f7f713966dbc2d88ac3b6f2
Sanjaycsk18 <sanjaycsk2004@gmail.com>
2024-06-24 20:13:22 +0530

    files are updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { Toaster } from './components/ui/toaster'
 import './index.css'
 import AuthProvider from './auth/AuthProvider';
-import Navbar from './components/ui/navbar';
 import Routes from './Routes';
-
 
 function App() {
 
   return (
       <>
           <div className='w-screen min-h-screen bg-gray-100 ' >
-            <Navbar/>
                 <AuthProvider>
                     <Routes />
                 </AuthProvider>
