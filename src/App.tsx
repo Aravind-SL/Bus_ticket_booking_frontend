24d9469f4ad3f7833e3ce79644983a8aa4b1281e
Sanjaycsk18 <sanjaycsk2004@gmail.com>
2024-06-26 13:20:29 +0530

    header and footer added

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import { Toaster } from './components/ui/toaster'
 import './index.css'
 import AuthProvider from './auth/AuthProvider';
 import Routes from './Routes';
+import NavBar from './components/ui/navbar';
+import {Footer} from './components/ui/footer';
 
 function App() {
-
   return (
-      <>
-          <div className='w-screen min-h-screen bg-gray-100 ' >
-                <AuthProvider>
-                    <Routes />
-                </AuthProvider>
-          </div>
-          <Toaster />
-      </>
+    <>
+      <NavBar />
+      <div className='w-screen min-h-screen bg-gray-100 '>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </div>
+      <Footer />
+      <Toaster />
+    </>
   )
 }
 
