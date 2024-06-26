f5c346c754bf305817da33da82acf1236781a675
Sanjaycsk18 <sanjaycsk2004@gmail.com>
2024-06-26 13:20:29 +0530

    header and footer added

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -5,15 +5,23 @@ import {
   } from '@/components/ui/menubar';
 import { Button } from '@/components/ui/button';
 import { useState } from 'react'; 
-import { redirect } from 'react-router-dom';
+
 
   
-export default function Navbar() {
+export default function NavBar() {
   const [Logged] = useState(false);
-    return (
-      <Menubar className='w-full h-250 flex items-center py-2 '>
+  return(
+      <Menubar className='w-full h-fit flex items-center font-medium '>
         <MenubarMenu>
-            <MenubarTrigger className='flex-auto md:flex-auto' >logo</MenubarTrigger>
+            <MenubarTrigger className='flex-auto md:flex-auto' >
+              <div className="sm:flex-auto sm:items-start sm:justify-between">
+                <a href="/home" className="flex items-start mb-2 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                    <img className='w-14 h-14 md:w-12 mb-2 ' src="https://img.freepik.com/premium-vector/logo-bus-icon-school-bus-vector-isolated-transport-bus-silhouette-design-black-bus_653669-3055.jpg " alt="BusKaro logo" />
+                    <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">BusKaro</span>
+                </a>
+              </div>
+            </MenubarTrigger> 
+            
             <MenubarTrigger>Home</MenubarTrigger>
             <MenubarTrigger>Browse</MenubarTrigger>
             <MenubarTrigger>Contact</MenubarTrigger>
@@ -24,6 +32,6 @@ export default function Navbar() {
             <MenubarTrigger><Button>Log In / Sign Up</Button></MenubarTrigger>
         </MenubarMenu>
       </Menubar>
-    );
+  );
 }
 
---
+++ src/home.tsx
@@ -0,0 +1,4 @@
+import NavBar from "./components/ui/navbar";
+import { Footer } from "./components/ui/footer";
+
+
