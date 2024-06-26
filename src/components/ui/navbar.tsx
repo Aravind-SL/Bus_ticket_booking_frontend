b404cee1f04c1b5215b548f4c6408a9419a4ade2
Sanjaycsk18 <sanjaycsk2004@gmail.com>
2024-06-26 13:31:23 +0530

    header and footer reconstructed

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -13,13 +13,12 @@ export default function NavBar() {
   return(
       <Menubar className='w-full h-fit flex items-center font-medium '>
         <MenubarMenu>
-            <MenubarTrigger className='flex-auto md:flex-auto' >
-              <div className="sm:flex-auto sm:items-start sm:justify-between">
-                <a href="/home" className="flex items-start mb-2 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <img className='w-14 h-14 md:w-12 mb-2 ' src="https://img.freepik.com/premium-vector/logo-bus-icon-school-bus-vector-isolated-transport-bus-silhouette-design-black-bus_653669-3055.jpg " alt="BusKaro logo" />
-                    <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">BusKaro</span>
-                </a>
-              </div>
+            <MenubarTrigger className='flex-auto md:flex-auto' ><div className="sm:flex-auto sm:items-start sm:justify-between">
+              <a href="/home" className="flex items-start mb-2 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                  <img className='w-14 h-14 md:w-12 mb-2 ' src="https://img.freepik.com/premium-vector/logo-bus-icon-school-bus-vector-isolated-transport-bus-silhouette-design-black-bus_653669-3055.jpg " alt="BusKaro logo" />
+                  <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">BusKaro</span>
+              </a>
+            </div>
             </MenubarTrigger> 
             
             <MenubarTrigger>Home</MenubarTrigger>
